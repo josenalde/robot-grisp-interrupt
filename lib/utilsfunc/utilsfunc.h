a400a11e074aba6e532ae574a065d2b0c2098a97
josenaldeOliveira <josenalde@gmail.com>
2025-04-27 11:22:44 -0300

    remove timing DEFINES

--- lib/utilsfunc/utilsfunc.h
+++ lib/utilsfunc/utilsfunc.h
@@ -7,9 +7,7 @@
 #define BTN_P2 18
 #define BTN_P3 12
 #define GRISP_PIN 4
-#define TIME_TO_POINT 1500
 #define RESET 0x00
-#define TEMPO_DEBOUNCE 100 * 1000 //ms
 #define BASE_MOTOR_ANGLE 90
 #include <ESP32Servo.h>
 extern long startPoint;
